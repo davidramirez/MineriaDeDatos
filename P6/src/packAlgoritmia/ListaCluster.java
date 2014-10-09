afa3b820852b3d16353dfcd2c499c4bec6166fe2
AndoniMartin <amartin108@ehu.es>
2014-10-09 11:11:30 +0200

    ListaCluster: Creada clase.

--- P6/src/packAlgoritmia/ListaCluster.java
+++ P6/src/packAlgoritmia/ListaCluster.java
@@ -1,5 +1,22 @@
 package packAlgoritmia;
 
+import java.util.LinkedList;
+
 public class ListaCluster {
+
+	private LinkedList<Instancia> listaCluster=new LinkedList<Instancia>();
+
+	/*
+	 * GETTERS & SETTERS
+	 */
+	
+	
+	public LinkedList<Instancia> getListaCluster() {
+		return listaCluster;
+	}
+
+	public void setListaCluster(LinkedList<Instancia> listaCluster) {
+		this.listaCluster = listaCluster;
+	} 
 
 }
