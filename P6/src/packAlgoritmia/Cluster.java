01efab5071758e31d13fddbc34af04c280a7613b
AndoniMartin <amartin108@ehu.es>
2014-10-09 10:33:20 +0200

    Cluster: Creada clase con parámetros y los prototipos de las funciones.

--- P6/src/packAlgoritmia/Cluster.java
+++ P6/src/packAlgoritmia/Cluster.java
@@ -1,5 +1,54 @@
 package packAlgoritmia;
 
 public class Cluster {
+ private Instancia centroide;
+ private ListaInstancias listaInstancias;
+ private double error;
+ 
+ public Cluster(ListaInstancias pListaInstancias)
+ {
+	 this.setListaInstancias(pListaInstancias);
+	 this.centroide=calcularCentroide();
+	 this.error=calcularError();
+ }
 
+private double calcularError() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+private Instancia calcularCentroide() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+/*
+ * GETTERS & SETTERS
+ */
+
+public Instancia getCentroide() {
+	return centroide;
+}
+
+
+public void setCentroide(Instancia centroide) {
+	this.centroide = centroide;
+}
+
+public ListaInstancias getListaInstancias() {
+	return listaInstancias;
+}
+
+public void setListaInstancias(ListaInstancias listaInstancias) {
+	this.listaInstancias = listaInstancias;
+}
+
+public double getError() {
+	return error;
+}
+
+public void setError(double error) {
+	this.error = error;
+}
 }
