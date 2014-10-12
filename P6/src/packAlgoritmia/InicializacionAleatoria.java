82595f98c0b77c75fd6a719d2845fa36f9af32a9
AndoniMartin <amartin108@ehu.es>
2014-10-12 12:36:28 +0200

    Inicialización aleatoria: Adaptada a herencia de Kmeans

--- P6/src/packAlgoritmia/InicializacionAleatoria.java
+++ P6/src/packAlgoritmia/InicializacionAleatoria.java
@@ -3,32 +3,106 @@ package packAlgoritmia;
 import java.util.Vector;
 
 public class InicializacionAleatoria extends KMeans {
-	Instancia[] centroides;
 	
-	public InicializacionAleatoria(ListaInstancias pListaInstancias, int pNumeroClusters)
+	Instancia[] centroides;
+	private int k;
+	private Distancia distancia;
+	private ListaInstancias instancias;
+	private int numIteraciones;
+	private double delta;
+	
+	
+
+	public InicializacionAleatoria(int pK, Distancia pDistancia, ListaInstancias pListaInstancias, int pNumIt, double pDelta)
 	{
-		double maximos[];
-		double minimos[];
-		
-		maximos=pListaInstancias.getMaximos();
-		minimos=pListaInstancias.getMinimos();
-		
-		int dimension=pListaInstancias.getListaInstancia().get(0).dimension();
-		double instanciaAleatoria[] = new double[pListaInstancias.dimension()];
-		Vector<Double> vectorTemporal = new Vector<Double>();
-		ListaInstancias centroides=new ListaInstancias();
-		for(int i=0;i<pNumeroClusters;i++)
-		{
-			for(int j=0;j<dimension;j++)
-			{
-				instanciaAleatoria[j] = Math.random()*(maximos[j]-minimos[j])+minimos[j];
-			}
-			for(int k=0;k<dimension;k++)
-			{
-				vectorTemporal.add(instanciaAleatoria[k]);
-			}
-			centroides.getListaInstancia().add(new Instancia(vectorTemporal));
-		}
+		super(pK,pDistancia,pListaInstancias,pNumIt,pDelta);
 	}
+	
+	
+public void inicializar()
+{
+	double maximos[];
+	double minimos[];
+	
+	maximos=this.getInstancias().getMaximos();
+	minimos=this.getInstancias().getMinimos();
+	
+	int dimension=this.getInstancias().getListaInstancia().get(0).dimension();
+	double instanciaAleatoria[] = new double[this.getInstancias().dimension()];
+	Vector<Double> vectorTemporal = new Vector<Double>();
+	ListaInstancias centroides=new ListaInstancias();
+	for(int i=0;i<this.k;i++)
+	{
+		for(int j=0;j<dimension;j++)
+		{
+			instanciaAleatoria[j] = Math.random()*(maximos[j]-minimos[j])+minimos[j];
+		}
+		for(int k=0;k<dimension;k++)
+		{
+			vectorTemporal.add(instanciaAleatoria[k]);
+		}
+		centroides.getListaInstancia().add(new Instancia(vectorTemporal));
+	}
+}
+
+public Instancia[] getCentroides() {
+	return centroides;
+}
+
+
+public void setCentroides(Instancia[] centroides) {
+	this.centroides = centroides;
+}
+
+
+public int getK() {
+	return k;
+}
+
+
+public void setK(int k) {
+	this.k = k;
+}
+
+
+public Distancia getDistancia() {
+	return distancia;
+}
+
+
+public void setDistancia(Distancia distancia) {
+	this.distancia = distancia;
+}
+
+
+public ListaInstancias getInstancias() {
+	return instancias;
+}
+
+
+public void setInstancias(ListaInstancias instancias) {
+	this.instancias = instancias;
+}
+
+
+public int getNumIteraciones() {
+	return numIteraciones;
+}
+
+
+public void setNumIteraciones(int numIteraciones) {
+	this.numIteraciones = numIteraciones;
+}
+
+
+public double getDelta() {
+	return delta;
+}
+
+
+public void setDelta(double delta) {
+	this.delta = delta;
+}
+
 	
 }
