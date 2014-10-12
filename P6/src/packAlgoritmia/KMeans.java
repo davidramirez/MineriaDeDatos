de9274c247c8cef8c24aa7f9523f8ace39f8e107
david <davidramirez_28@hotmail.com>
2014-10-12 12:24:57 +0200

    kmeans con la constructora y métodos

--- P6/src/packAlgoritmia/KMeans.java
+++ P6/src/packAlgoritmia/KMeans.java
@@ -1,5 +1,49 @@
 package packAlgoritmia;
 
 public abstract class KMeans {
+	
+	private int k;
+	private Distancia distancia;
+	private ListaInstancias instancias;
+	private int numIteraciones;
+	private double delta;
+	
+	public KMeans(int pK, Distancia pDistancia, ListaInstancias pListaInstancias, int pNumIt, double pDelta)
+	{
+		k = pK;
+		distancia = pDistancia;
+		instancias = pListaInstancias;
+		numIteraciones = pNumIt;
+		delta = pDelta;
+	}
+	
+	public ListaCluster ejecutar()
+	{
+		ListaCluster lista = null;
+				
+		this.inicializar();
+		
+		//while
+		
+			this.calcularPertenencias();
+			
+			this.calcularCentroides();
+			
+		
+		return lista;	
+			
+	}
+	
+	protected abstract void inicializar();
+	
+	public void calcularPertenencias()
+	{
+		
+	}
+	
+	public void calcularCentroides()
+	{
+		
+	}
 
 }
