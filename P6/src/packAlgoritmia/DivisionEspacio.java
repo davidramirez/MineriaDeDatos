8a5592d496827e184c1179e7413e439858bbdc79
AndoniMartin <amartin108@ehu.es>
2014-10-14 16:30:43 +0200

    Creada la inicialización de división del espacio.

--- P6/src/packAlgoritmia/DivisionEspacio.java
+++ P6/src/packAlgoritmia/DivisionEspacio.java
@@ -1,7 +1,10 @@
 package packAlgoritmia;
 
+import java.util.Vector;
+
 import packDistancias.Distancia;
 import packFicheros.CargadorFichero;
+import packInstancias.Instancia;
 import packInstancias.ListaInstancias;
 
 public class DivisionEspacio extends KMeans {
@@ -10,9 +13,28 @@ public class DivisionEspacio extends KMeans {
 		super(pK, pDistancia, pListaInstancias, pNumIt, pDelta);
 	}
 
-	@Override
+	/**
+	 * Inicialización de KMeans que obtiene los parámetros usando una interpretación propia del algoritmo divisor del espacio que divide cada dimensión espacial es K partes y asigna el centro de cada parte a un atributo del atributo del vector de centroides.
+	 * 
+	 */
 	protected void inicializar() {
-		// TODO Auto-generated method stub
+		double maximos[];
+		double minimos[];
+		
+		maximos=this.getInstancias().getMaximos();
+		minimos=this.getInstancias().getMinimos();
+		
+		int dimensionEspacial=this.getInstancias().getDimension();
+		Vector<Double> instanciaTemporal=new Vector<Double>();
+		for(int indice=0;indice<dimensionEspacial;indice++)
+		{
+			for(int indiceAtributos=0;indiceAtributos<dimensionEspacial;indiceAtributos++)
+			{
+				instanciaTemporal.add(((maximos[indiceAtributos]-minimos[indiceAtributos])/(this.k))-this.k/2);
+			}
+			this.centroides[indice]=new Instancia(instanciaTemporal);
+		}
+		
 	}
 
 }
